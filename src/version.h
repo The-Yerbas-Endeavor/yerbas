cf2ead892511a0fa3e7a392d3fb026e73c998669
mrx <yerbascore@gmail.com>
2022-05-24 09:41:18 -0700

    fix

--- src/version.h
+++ src/version.h
@@ -1,3 +1,16 @@
+// Copyright (c) 2012-2014 The Bitcoin Core developers
+// Copyright (c) 2014-2021 The Dash Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_VERSION_H
+#define BITCOIN_VERSION_H
+
+/**
+ * network protocol versioning
+ */
+
+
 static const int PROTOCOL_VERSION = 70219;
 
 //! initial proto version, to be increased after version/verack negotiation
