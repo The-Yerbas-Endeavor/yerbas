37fe0df0f27974addc8ce65d970aa00500246a49
nandofw <dev.nandofw@gmail.com>
2022-07-09 16:53:49 -0400

    updates

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -3,12 +3,12 @@ packages:=boost openssl libevent zeromq gmp chia_bls backtrace
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 qt_arm_linux_packages:=$(qt_x86_64_linux_packages)
 qt_aarch64_linux_packages:=$(qt_x86_64_linux_packages)
 
-
+qt_darwin_packages=qt
 qt_mingw32_packages=qt
 
 wallet_packages=bdb
