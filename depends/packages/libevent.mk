d3af57c62f5819d51e9bea87fd3a638a68f3ace2
anno <yerbascore@gmail.com>
2023-06-11 16:45:03 -0700

    Ubuntu 22

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -1,12 +1,4 @@
 package=libevent
-$(package)_version=2.1.8
-$(package)_download_path=https://github.com/libevent/libevent/releases/download/release-$($(package)_version)-stable
-$(package)_file_name=$(package)-$($(package)_version)-stable.tar.gz
-$(package)_sha256_hash=965cc5a8bb46ce4199a47e9b2c9e1cae3b137e8356ffdad6d94d3b9069b71dc2
-
-define $(package)_preprocess_cmds
-  ./autogen.sh
-endef
 $(package)_version=2.1.12-stable
 $(package)_download_path=https://github.com/libevent/libevent/releases/download/release-$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
