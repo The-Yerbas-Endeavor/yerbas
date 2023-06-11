97b320bd6a750363516627c93b92cba68cb01d43
anno <yerbascore@gmail.com>
2023-06-11 16:20:22 -0700

    Ubuntu 22

--- src/bls/bls.h
+++ src/bls/bls.h
@@ -11,12 +11,12 @@
 #include "uint256.h"
 #include "utilstrencodings.h"
 
-#undef ERROR // chia BLS uses relic, which defines ERROR, which in turn causes win32/win64 builds to print many warnings
-#include <chiabls/bls.hpp>
-#include <chiabls/privatekey.hpp>
-#include <chiabls/publickey.hpp>
-#include <chiabls/signature.hpp>
-#undef DOUBLE
+#undef ERROR // BLS uses relic, which defines ERROR, which in turn causes win32/win64 builds to print many warnings
+#include <bls-dash/bls.hpp>
+#include <bls-dash/privatekey.hpp>
+#include <bls-dash/elements.hpp>
+#include <bls-dash/schemes.hpp>
+#include <bls-dash/threshold.hpp>
 
 #include <array>
 #include <mutex>
