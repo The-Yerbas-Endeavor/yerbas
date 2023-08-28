24427ab156985788923db73e294eb740df7e65b3
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2023-08-27 21:59:55 -0700

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -3121,7 +3121,6 @@ bool static ConnectTip(CValidationState& state, const CChainParams& chainparams,
 
         /** YERBAS ASSETS START */
         bool assetFlushed = assetCache.Flush();
-        assert(assetFlushed);
         /** YERBAS ASSETS END */
     }
     int64_t nTime4 = GetTimeMicros(); nTimeFlush += nTime4 - nTime3;
