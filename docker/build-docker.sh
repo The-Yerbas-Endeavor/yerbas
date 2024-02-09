fc8c5ddf413ad0946b3a329e726c72d7fe661a0e
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2024-02-08 19:45:22 -0800

    Update build-docker.sh

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -14,7 +14,9 @@ rm docker/bin/*
 mkdir docker/bin
 cp $BUILD_DIR/src/yerbasd docker/bin/
 cp $BUILD_DIR/src/yerbas-cli docker/bin/
+cp $BUILD_DIR/src/yerbas-tx docker/bin/
 strip docker/bin/yerbasd
 strip docker/bin/yerbas-cli
+strip docker/bin/yerbas-tx
 
 docker build --pull -t $DOCKER_IMAGE:$DOCKER_TAG -f docker/Dockerfile docker
