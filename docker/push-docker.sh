dffcbb957ce6b93d082c132585386a76bed39265
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2024-02-08 17:50:11 -0800

    Update push-docker.sh

--- docker/push-docker.sh
+++ docker/push-docker.sh
@@ -5,7 +5,7 @@ export LC_ALL=C
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/.. || exit
 
-DOCKER_IMAGE=${DOCKER_IMAGE:-The-Yerbas-Endeavor/yerbasd-develop}
+DOCKER_IMAGE=${DOCKER_IMAGE:-Yerbman/yerbas}
 DOCKER_TAG=${DOCKER_TAG:-latest}
 
 if [ -n "$DOCKER_REPO" ]; then
