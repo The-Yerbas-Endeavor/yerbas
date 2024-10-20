f3427f50e1d48227fe83a2587abc7abd65dcf55c
The-Yerbas-Endeavor <yerbascore@gmail.com>
2024-10-20 14:34:13 -0700

    add util

--- src/batchedlogger.cpp
+++ src/batchedlogger.cpp
@@ -6,7 +6,7 @@
 #include "batchedlogger.h"
 #include "util.h"
 
-CBatchedLogger::CBatchedLogger(uint64_t _category, const std::string& _header) :
+CBatchedLogger::CBatchedLogger(BCLog::LogFlags _category, const std::string& _header) :
     accept(LogAcceptCategory(_category)), header(_header)
 {
 }
